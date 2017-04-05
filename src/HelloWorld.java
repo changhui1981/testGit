880b66eb385b9b6c82aa0439caf9a9cd5f50cdd2
changhui <chinese_power@163.com>
2017-04-05 18:00:29 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Description:
+ * Created by ChangHui on 2017/4/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
